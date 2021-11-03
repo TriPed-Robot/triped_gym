96a0915fb1be9e241455407816da0bd10d8be66f
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-11-03 19:27:56 +0100

    finished trip inverse kinematics integration

--- src/triped_sim.py
+++ src/triped_sim.py
@@ -37,10 +37,17 @@ class SimplifiedTriped:
                                 ('leg2_gimbal_joint', 'rz'),
                                 ('leg2_extend_joint', 'ry'), ]
 
+        self.inv_kin_solver = [trip.SimpleInvKinSolver(
+            triped, 'leg0_A_LL_Joint_FCS', update_robot=False),
+            trip.SimpleInvKinSolver(
+            triped, 'leg1_A_LL_Joint_FCS', update_robot=False),
+            trip.SimpleInvKinSolver(
+            triped, 'leg2_A_LL_Joint_FCS', update_robot=False)]
+
         # disable the default velocity motors
         # and set some position control with small force
         #  to emulate joint friction/return to a rest pose
-        self.max_joint_force = 10000*np.ones(p.getNumJoints(self.urdf))
+        self.max_joint_force = 800*np.ones(p.getNumJoints(self.urdf))
         for joint in range(p.getNumJoints(self.urdf)):
             p.resetJointState(self.urdf, joint, targetValue=0)
 
@@ -59,20 +66,20 @@ class SimplifiedTriped:
                                           ] = p.getJointState(self.urdf, joint)[0]
         return virtual_state
 
-    def set_virtual_state(self, state: Dict[str, Dict[str, float]]):
+    def set_virtual_state(self, target: Dict[str, Dict[str, float]]):
         """Sets the target position of each joint following Trip_kinematics conventions.
         The maximum force of each joint is set according to the max_joint_force class attribute. 
 
         Args:
-            state (Dict[str, Dict[str, float]]): valid joint states, note that not all states need
+            target (Dict[str, Dict[str, float]]): valid joint states, note that not all states need
                                                  to be supplied.
 
         Raises:
             ValueError: If the specified joint state is not valid
         """
 
-        if all(key in self.joint_targets.keys()for key in state.keys()):
-            for joint, joint_state in state.items():
+        if all(key in self.joint_targets.keys()for key in target.keys()):
+            for joint, joint_state in target.items():
                 for key, value in joint_state.items():
                     state_tuple = (joint, key)
                     joint_number = self._joint_mappings.index(state_tuple)
@@ -83,6 +90,18 @@ class SimplifiedTriped:
             raise ValueError('Error: One or more keys are not part of the triped state. ' +
                              'correct keys are: '+str(self.joint_targets.keys()))
 
+    def set_foot_position(self, leg_number, target):
+        """Allows the position control of a leg of the TriPed.
+        Kinematic calculations are performed using TriP, although pybullet is also capable 
+        of computing inverse kinematics.
+
+        Args:
+            leg_number ([type]): The leg which is to be controlled, numbered from zero to two.
+            target ([type]): A 3 dimensional target position.
+        """
+        solution = self.inv_kin_solver[leg_number].solve_virtual(target)
+        self.set_virtual_state(solution)
+
 
 if __name__ == "__main__":
     physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
@@ -90,19 +109,14 @@ if __name__ == "__main__":
     p.setGravity(0, 0, -9.81)
     p.setPhysicsEngineParameter(numSolverIterations=1000)
     planeId = p.loadURDF("plane.urdf")
-    startPos = [0, 0, 1.5]
+    startPos = [0, 0, 1]
     startOrientation = p.getQuaternionFromEuler([0, 0, 0])
 
     robot = SimplifiedTriped(startPos, startOrientation)
 
-    inv_kin_solver = [trip.SimpleInvKinSolver(triped, 'leg0_A_LL_Joint_FCS', update_robot=False),
-                      trip.SimpleInvKinSolver(
-                          triped, 'leg1_A_LL_Joint_FCS', update_robot=False),
-                      trip.SimpleInvKinSolver(triped, 'leg2_A_LL_Joint_FCS', update_robot=False)]
-
-    initial_pos_0 = np.array([0.4, 0,  -0.6])
-    initial_pos_1 = np.array([-0.2025, -0.35074,  -0.6])
-    initial_pos_2 = np.array([-0.2025,  0.35074,  -0.6])
+    initial_pos_0 = np.array([0.4*1.2, 0,  -0.6])
+    initial_pos_1 = np.array([-0.2025*1.2, -0.35074*1.2,  -0.6])
+    initial_pos_2 = np.array([-0.2025*1.2,  0.35074*1.2,  -0.6])
 
     poses = [initial_pos_0, initial_pos_1, initial_pos_2]
 
@@ -110,13 +124,8 @@ if __name__ == "__main__":
         p.stepSimulation()
         for leg_number in [0, 1, 2]:
             # + 0.3 * (1-max(1, i/1000))
-            pos_with_height = poses[leg_number] + 0.2 * \
-                np.array([np.cos(i/1000), np.sin(i/1000), 0])
-            solution = inv_kin_solver[leg_number].solve_virtual(
-                target=pos_with_height, initial_tip=triped.get_virtual_state())
-            triped.set_virtual_state(solution)
-            print(solution)
-            robot.set_virtual_state({'leg'+str(leg_number)+'_gimbal_joint': solution['leg'+str(leg_number)+'_gimbal_joint'],
-                                     'leg'+str(leg_number)+'_extend_joint': solution['leg'+str(leg_number)+'_extend_joint']})
+            pos_with_height = poses[leg_number] - 0.1 * \
+                np.array([np.cos(i/200), np.sin(i/200), 0])
+            robot.set_foot_position(leg_number, pos_with_height)
         time.sleep(1./240.)
     p.disconnect()
