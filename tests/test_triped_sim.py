f74cfe5f51f70178b658014528cc8263144f2a8f
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-11-17 12:49:43 +0100

    made test pytest executable

--- tests/test_triped_sim.py
+++ tests/test_triped_sim.py
@@ -1,21 +1,15 @@
-import time
 import pybullet as p
-import pybullet_data
 import os
 import csv
 from triped_sim import Triped
 import numpy as np
 
-if __name__ == "__main__":
-    physics_client = p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.setGravity(0, 0, -9.81)
-    p.setPhysicsEngineParameter(numSolverIterations=1000)
-    planeId = p.loadURDF("plane.urdf")
-    start_position = [0, 0, 1]
-    start_orientation = p.getQuaternionFromEuler([0, 0, 0])
 
-    robot = Triped(start_position, start_orientation)
+def test_triped_sim():
+    physics_client = p.connect(p.DIRECT)
+    p.setPhysicsEngineParameter(numSolverIterations=1000)
+    start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+    robot = Triped([0, 0, 0], start_orientation)
 
     p.createConstraint(robot.urdf,
                        -1, -1, -1,
@@ -56,9 +50,9 @@ if __name__ == "__main__":
             input_t1_tip.append([float(row[0]), float(row[3]), float(row[6])])
             input_t2_tip.append([float(row[2]), float(row[5]), float(row[8])])
 
-    p.setRealTimeSimulation(1)
     for i in range(len(input_x)):
         row = []
+        # set the desired foot positions
         for leg_number in [0, 1, 2]:
             tip['leg_'+str(leg_number) +
                 '_swing_left'] = input_t1_tip[i][leg_number]
@@ -72,19 +66,22 @@ if __name__ == "__main__":
                                      input_y[i][leg_number],
                                      input_z[i][leg_number]])
 
-            leg_row = robot.get_actuated_state()
+        # update the simulation multiple times.
+        for i in range(20):
+            p.stepSimulation()
 
+        # check if the correct actuated state has been reached
+        leg_row = robot.get_actuated_state()
+        for leg_number in [0, 1, 2]:
             row.extend([leg_row['leg_'+str(leg_number)+'_swing_left'],
                        leg_row['leg_'+str(leg_number)+'_extend_joint_ry'],
                        leg_row['leg_'+str(leg_number)+'_swing_right']])
         calculated.append(row)
-        time.sleep(1./400.)
 
     p.disconnect()
 
-    precision = 0.03
+    # precision is tied to the number of control steps the simulation can perform
+    precision = 0.4
     sample_results = [(np.abs(reference[i]-calculated[i]) <
-                       precision).all() for i in range(len(reference))]
-    # sample_results = [np.abs(reference[i]-calculated[i])
-    #                  for i in range(len(reference))]
-    print(sample_results)
+                      precision).all() for i in range(1, len(reference))]
+    assert all(sample_results)
