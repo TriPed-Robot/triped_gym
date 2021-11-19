ee32a360654652559bcc3e63fd5002adb03e775f
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-11-19 14:36:21 +0100

    tested rendering using naive plt.pause

--- tests/env_test.py
+++ tests/env_test.py
@@ -2,19 +2,23 @@ import gym
 import triped_gym
 from triped_gym.utility import render_image
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+
 env = gym.make('TriPed-v0')
 
 obs = env.reset()
 print("The initial observation is {}".format(obs))
 
-cam_dist = 3
+cam_dist = 2
 cam_yaw = 0
 render_width = 320
 cam_pitch = -30
 render_height = 240
 
+
 # Sample a random action from the entire action space
-for i in range(1000):
+for i in range(20):
     random_action = env.action_space.sample()
 
     # # Take the action and get the new observation spacepi
@@ -22,6 +26,7 @@ for i in range(1000):
     print("The new observation is {}".format(new_obs))
 
     base_pos, _ = env.robot.get_world_state()
-    rpg_img = render_image(base_pos, cam_dist, cam_yaw,
-                           cam_pitch, render_width, render_height)
+    plt.imshow(render_image(base_pos, cam_dist, cam_yaw,
+                            cam_pitch, render_width, render_height))
+    plt.pause(0.0001)
     time.sleep(1/240.)
