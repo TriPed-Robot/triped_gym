3618a7b974c49637e3566044a3688febe5713715
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-11-22 08:23:23 +0100

    packaged rendering into animation update

--- tests/env_test.py
+++ tests/env_test.py
@@ -3,6 +3,7 @@ import triped_gym
 from triped_gym.utility import render_image
 import time
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 
 env = gym.make('TriPed-v0')
@@ -16,17 +17,25 @@ render_width = 320
 cam_pitch = -30
 render_height = 240
 
+fig = plt.figure()
+base_pos, _ = env.robot.get_world_state()
+camera_feed = plt.imshow(render_image(base_pos, cam_dist, cam_yaw,
+                                      cam_pitch, render_width, render_height))
+
 
 # Sample a random action from the entire action space
-for i in range(20):
+def update(i):
     random_action = env.action_space.sample()
 
     # # Take the action and get the new observation spacepi
-    new_obs, reward, done, info = env.step(random_action)
+    new_obs, _, _, _ = env.step(random_action)
     print("The new observation is {}".format(new_obs))
 
     base_pos, _ = env.robot.get_world_state()
-    plt.imshow(render_image(base_pos, cam_dist, cam_yaw,
-                            cam_pitch, render_width, render_height))
-    plt.pause(0.0001)
-    time.sleep(1/240.)
+    camera_feed.set_array(render_image(base_pos, cam_dist, cam_yaw,
+                                       cam_pitch, render_width, render_height))
+    return camera_feed
+
+
+animation = FuncAnimation(fig, update, range(0, 20), blit=True)
+plt.show()
