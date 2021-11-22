f8be6e4163de87ff74167a7e08b07e05108dc534
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-11-22 14:25:30 +0100

    closed environment to allow for congruent testing

--- tests/test_gym_environments.py
+++ tests/test_gym_environments.py
@@ -1,21 +1,22 @@
-from math import exp
-import gym
-import triped_gym
 import os
-import csv
+import gym
 import numpy as np
+import pybullet as p
+import triped_gym
 
 # test environment TriPed-v0
 
 
 def test_step_causality():
+    """This function tests wheter actions have the desired 
+       causal effect on the foot position observation.
+    """
     precision = 0.12
 
     action_consequence_causal = True
 
     env = gym.make('TriPed-v0')
-    env.set_timestep_length(0.1)
-    prev_obs = env.reset()
+    env.reset()
 
     forward_reference = open(os.path.join(
         'tests', 'data', 'endeffector_coordinates.csv'))
@@ -25,8 +26,6 @@ def test_step_causality():
     expected_foot_pos = np.loadtxt(forward_reference, delimiter=",")
     action = np.loadtxt(inverse_reference, delimiter=",")
 
-    env._position_control_freq = 600
-
     for i in range(len(action)):
         obs, _, _, _ = env.step(action[i])
         foot_pos = obs[3:12]
@@ -34,7 +33,5 @@ def test_step_causality():
         action_consequence_causal = action_consequence_causal and np.linalg.norm(
             foot_pos-expected_foot_pos[i]) <= precision
 
+    env.close()
     assert action_consequence_causal
-
-
-test_step_causality()
